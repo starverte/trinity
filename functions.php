9bd4a2581d73c39d6e0b8c49357b231287d8e8b2
Matt Beall <mbeall@starverte.com>
2014-04-16 15:36:11 -0600

    Register new nav_menus in functions.php

---
+++ functions.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Trinity functions and definitions
+ *
+ * @package Flint/Trinity
+ * @since 1.0
+ */
+
+/**
+ * Sets up theme defaults and registers support for various WordPress features.
+ */
+function trinity_after_setup_theme() {
+  
+  unregister_nav_menu( 'primary' );
+  
+  register_nav_menus( array(
+    'screen-lg' => __( 'Large Desktops', 'trinity' ),
+    'screen-md' => __( 'Desktops'      , 'trinity' ),
+    'screen-sm' => __( 'Tablets'       , 'trinity' ),
+    'screen-xs' => __( 'Phones'        , 'trinity' ),
+  ) );
+
+}
+add_action( 'after_setup_theme', 'trinity_after_setup_theme', 20 );
