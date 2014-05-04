36f67ff7242075cace073b7334e5f04655a50a7a
Matt Beall <mbeall@starverte.com>
2014-05-03 23:50:23 -0600

    Remove spaces from empty lines

--- functions.php
+++ functions.php
@@ -10,9 +10,9 @@
  * Sets up theme defaults and registers support for various WordPress features.
  */
 function trinity_after_setup_theme() {
-  
+
   unregister_nav_menu( 'primary' );
-  
+
   register_nav_menus( array(
     'screen-lg' => __( 'Large Desktops', 'trinity' ),
     'screen-md' => __( 'Desktops'      , 'trinity' ),
--- header-nav.php
+++ header-nav.php
@@ -9,29 +9,29 @@
  */
 ?>
   <div class="container paper">
-  
+
     <nav class="visible-xs menu" role="navigation">
       <h1 class="screen-reader-text"><?php _e( 'Menu', 'flint' ); ?></h1>
       <div class="screen-reader-text skip-link"><a href="#content" title="<?php esc_attr_e( 'Skip to content', 'flint' ); ?>"><?php _e( 'Skip to content', 'flint' ); ?></a></div>
       <?php wp_nav_menu( array( 'theme_location' => 'screen-xs', 'container' => false, 'menu_class' => 'nav menu-nav', 'fallback_cb' => false, 'walker' => new Flint_Bootstrap_Menu ) ); ?>
     </nav><!-- .navbar -->
-    
+
     <nav class="visible-sm navbar navbar-trinity" role="navigation">
       <h1 class="screen-reader-text"><?php _e( 'Menu', 'flint' ); ?></h1>
       <div class="screen-reader-text skip-link"><a href="#content" title="<?php esc_attr_e( 'Skip to content', 'flint' ); ?>"><?php _e( 'Skip to content', 'flint' ); ?></a></div>
       <?php wp_nav_menu( array( 'theme_location' => 'screen-sm', 'container' => false, 'menu_class' => 'nav navbar-nav', 'fallback_cb' => false, 'walker' => new Flint_Bootstrap_Menu ) ); ?>
     </nav><!-- .navbar -->
-    
+
     <nav class="visible-md navbar navbar-trinity" role="navigation">
       <h1 class="screen-reader-text"><?php _e( 'Menu', 'flint' ); ?></h1>
       <div class="screen-reader-text skip-link"><a href="#content" title="<?php esc_attr_e( 'Skip to content', 'flint' ); ?>"><?php _e( 'Skip to content', 'flint' ); ?></a></div>
       <?php wp_nav_menu( array( 'theme_location' => 'screen-md', 'container' => false, 'menu_class' => 'nav navbar-nav', 'fallback_cb' => false, 'walker' => new Flint_Bootstrap_Menu ) ); ?>
     </nav><!-- .navbar -->
-    
+
     <nav class="visible-lg navbar navbar-trinity" role="navigation">
       <h1 class="screen-reader-text"><?php _e( 'Menu', 'flint' ); ?></h1>
       <div class="screen-reader-text skip-link"><a href="#content" title="<?php esc_attr_e( 'Skip to content', 'flint' ); ?>"><?php _e( 'Skip to content', 'flint' ); ?></a></div>
       <?php wp_nav_menu( array( 'theme_location' => 'screen-lg', 'container' => false, 'menu_class' => 'nav navbar-nav', 'fallback_cb' => false, 'walker' => new Flint_Bootstrap_Menu ) ); ?>
     </nav><!-- .navbar -->
-  
+
   </div><!-- .container -->
