d1d365af19cc20de72f5904c7168e66fc6224fee
Matt Beall <mbeall@starverte.com>
2014-05-03 23:46:58 -0600

    Footer column fix for smaller screens

--- footer.php
+++ footer.php
@@ -13,10 +13,10 @@
 
 <footer id="colophon" class="site-footer" role="contentinfo">
   <div class="site-info container">
-    <div id="custom-footer" class="col-lg-6 col-md-6">
+    <div id="custom-footer" class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
       <?php flint_custom_footer(); ?>
     </div>
-    <div id="credits" class="col-lg-6 col-md-6">
+    <div id="credits" class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
       <?php $theme = wp_get_theme(); ?>
       Proudly powered by <a href="http://wordpress.org/" title="A Semantic Personal Publishing Platform">WordPress</a><br />
       Theme: <a href="<?php echo $theme->get( 'ThemeURI' ) ?>"><?php echo $theme ?></a> by <?php echo $theme->get( 'Author' ) ?>
