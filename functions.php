3668b2e7f601332f488437c610ad511d10ac399d
Matt Beall <mbeall@starverte.com>
2014-08-23 15:27:31 -0600

    LifePointe-specific changes
    - Add theme support for post-thumbnails
    - Set post thumbnail size
    - Add custom image sizes for LifePointe website

--- functions.php
+++ functions.php
@@ -19,6 +19,19 @@ function trinity_after_setup_theme() {
     'screen-sm' => __( 'Tablets'       , 'trinity' ),
     'screen-xs' => __( 'Phones'        , 'trinity' ),
   ) );
+  
+  add_theme_support( 'post-thumbnails' );
+  set_post_thumbnail_size( 150, 150 );
+  
+  add_image_size('small-icon',   32,  32, true);
+  add_image_size('large-icon',   64,  64, true);
+  add_image_size('profile',     120, 150, true);
+  add_image_size('next-steps',  220, 125, true);
+  add_image_size('podcast',     300, 300, true);
+  add_image_size('small-title', 400, 225, true);
+  add_image_size('large-title', 640, 360, true);
 
 }
 add_action( 'after_setup_theme', 'trinity_after_setup_theme', 20 );
+
+
