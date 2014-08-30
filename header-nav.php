5fb74d5bb33f91da4e5bdb6566d6cbaa097a4f75
Matt Beall <mbeall@starverte.com>
2014-08-30 15:26:44 -0600

    Reset header-nav.php to use primary menu

--- header-nav.php
+++ header-nav.php
@@ -5,33 +5,29 @@
  * Displays the navigation menu
  *
  * @package Flint/Trinity
- * @since 1.0
+ * @since 0.0.0
  */
 ?>
-  <div class="nav-trinity">
 
-    <nav class="visible-xs-block menu" role="navigation">
-      <h1 class="screen-reader-text"><?php _e( 'Menu', 'flint' ); ?></h1>
-      <div class="screen-reader-text skip-link"><a href="#content" title="<?php esc_attr_e( 'Skip to content', 'flint' ); ?>"><?php _e( 'Skip to content', 'flint' ); ?></a></div>
-      <?php wp_nav_menu( array( 'theme_location' => 'screen-xs', 'container' => false, 'menu_class' => 'nav menu-nav', 'fallback_cb' => false, 'walker' => new Flint_Bootstrap_Menu ) ); ?>
-    </nav><!-- .navbar -->
+  <nav class="navbar navbar-trinity" role="navigation">
+    <h1 class="screen-reader-text"><?php _e( 'Menu', 'flint' ); ?></h1>
+    <div class="screen-reader-text skip-link"><a href="#content" title="<?php esc_attr_e( 'Skip to content', 'flint' ); ?>"><?php _e( 'Skip to content', 'flint' ); ?></a></div>
+    <div class="container">
+      <!-- Brand and toggle get grouped for better mobile display -->
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-primary-collapse">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand visible-xs-block" href="#" data-toggle="collapse" data-target=".navbar-primary-collapse">Menu</a>
+      </div><!-- .navbar-header -->
 
-    <nav class="visible-sm-block navbar navbar-trinity container" role="navigation">
-      <h1 class="screen-reader-text"><?php _e( 'Menu', 'flint' ); ?></h1>
-      <div class="screen-reader-text skip-link"><a href="#content" title="<?php esc_attr_e( 'Skip to content', 'flint' ); ?>"><?php _e( 'Skip to content', 'flint' ); ?></a></div>
-      <?php wp_nav_menu( array( 'theme_location' => 'screen-sm', 'container' => false, 'menu_class' => 'nav navbar-nav', 'fallback_cb' => false, 'walker' => new Flint_Bootstrap_Menu ) ); ?>
-    </nav><!-- .navbar -->
+      <!-- Collect the nav links, forms, and other content for toggling -->
+      <div class="collapse navbar-collapse navbar-primary-collapse">
+        <?php wp_nav_menu( array( 'theme_location' => 'primary', 'container' => false, 'menu_class' => 'nav navbar-nav', 'fallback_cb' => false, 'walker' => new Flint_Bootstrap_Menu ) ); ?>
+      </div><!-- .navbar-collapse -->
+    </div><!-- .container -->
+  </nav><!-- .navbar -->
 
-    <nav class="visible-md-block navbar navbar-trinity container" role="navigation">
-      <h1 class="screen-reader-text"><?php _e( 'Menu', 'flint' ); ?></h1>
-      <div class="screen-reader-text skip-link"><a href="#content" title="<?php esc_attr_e( 'Skip to content', 'flint' ); ?>"><?php _e( 'Skip to content', 'flint' ); ?></a></div>
-      <?php wp_nav_menu( array( 'theme_location' => 'screen-md', 'container' => false, 'menu_class' => 'nav navbar-nav', 'fallback_cb' => false, 'walker' => new Flint_Bootstrap_Menu ) ); ?>
-    </nav><!-- .navbar -->
-
-    <nav class="visible-lg-block navbar navbar-trinity container" role="navigation">
-      <h1 class="screen-reader-text"><?php _e( 'Menu', 'flint' ); ?></h1>
-      <div class="screen-reader-text skip-link"><a href="#content" title="<?php esc_attr_e( 'Skip to content', 'flint' ); ?>"><?php _e( 'Skip to content', 'flint' ); ?></a></div>
-      <?php wp_nav_menu( array( 'theme_location' => 'screen-lg', 'container' => false, 'menu_class' => 'nav navbar-nav', 'fallback_cb' => false, 'walker' => new Flint_Bootstrap_Menu ) ); ?>
-    </nav><!-- .navbar -->
-
-  </div><!-- .container -->
