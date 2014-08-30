12e41d2bce3607a5e10ff60c9f54d9d1fe3cb7f0
Matt Beall <mbeall@starverte.com>
2014-08-30 15:21:23 -0600

    Go back to primary menu and remove scriptaculous

--- functions.php
+++ functions.php
@@ -11,30 +11,11 @@
  */
 function trinity_after_setup_theme() {
 
-  unregister_nav_menu( 'primary' );
-
   register_nav_menus( array(
-    'screen-lg' => __( 'Large Desktops', 'trinity' ),
-    'screen-md' => __( 'Desktops'      , 'trinity' ),
-    'screen-sm' => __( 'Tablets'       , 'trinity' ),
-    'screen-xs' => __( 'Phones'        , 'trinity' ),
-    'footer1'   => __( 'Footer 1'      , 'trinity' ),
-    'footer2'   => __( 'Footer 2'      , 'trinity' ),
+    'footer1'   => __( 'Sitemap Left' , 'trinity' ),
+    'footer2'   => __( 'Sitemap Right', 'trinity' ),
   ) );
 
 }
 add_action( 'after_setup_theme', 'trinity_after_setup_theme', 20 );
 
-/**
- * Enqueue scripts and styles
- */
-function trinity_enqueue_scripts() {
-
-  /**
-   * Load scriptaculous
-   */
-  wp_enqueue_script('scriptaculous');
-  wp_enqueue_script('scriptaculous-effects');
-
-}
-add_action( 'wp_enqueue_scripts', 'trinity_enqueue_scripts', 20 );
