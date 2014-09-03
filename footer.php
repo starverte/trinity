a4c45928cf3d4b210a0fba5535710a14d850bf6a
Matt Beall <mbeall@starverte.com>
2014-09-03 15:14:29 -0600

    Update footer.php

--- footer.php
+++ footer.php
@@ -60,12 +60,12 @@
 
 <footer id="colophon" class="site-footer" role="contentinfo">
   <div class="site-info container">
-    <div id="custom-footer" class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+    <div id="custom-footer" class="col-xs-12 col-md-6">
       <?php flint_custom_footer(); ?>
     </div>
-    <div id="credits" class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+    <div id="credits" class="col-xs-12 col-md-6">
       <?php $theme = wp_get_theme(); ?>
-      Proudly powered by <a href="http://wordpress.org/" title="A Semantic Personal Publishing Platform">WordPress</a><br />
+      Powered by <a href="http://wordpress.org/" title="A Semantic Personal Publishing Platform">WordPress</a><br />
       Theme: <a href="<?php echo $theme->get( 'ThemeURI' ) ?>"><?php echo $theme ?></a> by <?php echo $theme->get( 'Author' ) ?>
     </div>
   </div><!-- .site-info -->
