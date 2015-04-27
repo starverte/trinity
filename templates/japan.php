bd280ce3031eb3de5891b63b88d64e157e288ee4
Matt Beall <mbeall@starverte.com>
2015-04-27 16:57:56 -0600

    New Japan page template

---
+++ templates/japan.php
@@ -0,0 +1,209 @@
+<?php
+/**
+ * Template Name: Japan
+ *
+ * @package Flint/Trinity
+ * @since 0.2
+ */
+
+get_header(); ?>
+<?php flint_get_widgets('header'); ?>
+
+  <div id="primary" class="content-area">
+
+    <div id="carousel-jp15" class="carousel slide" data-ride="carousel">
+      <!-- Indicators -->
+      <ol class="carousel-indicators">
+        <li data-target="#carousel-jp15" data-slide-to="0" class="active"></li>
+        <li data-target="#carousel-jp15" data-slide-to="1"></li>
+        <li data-target="#carousel-jp15" data-slide-to="2"></li>
+        <li data-target="#carousel-jp15" data-slide-to="3"></li>
+        <li data-target="#carousel-jp15" data-slide-to="4"></li>
+        <li data-target="#carousel-jp15" data-slide-to="5"></li>
+      </ol>
+
+      <!-- Wrapper for slides -->
+      <div class="carousel-inner" role="listbox">
+        <div class="item active">
+          <img src="http://lifepointe.starvertellc.netdna-cdn.com/wp-content/uploads/jp15_title_cover.jpg" alt="Japan 2015" id="jp15_title_cover">
+        </div>
+        <div class="item">
+          <img src="http://lifepointe.starvertellc.netdna-cdn.com/wp-content/uploads/jp15_megan_cover.jpg" alt="Megan Spiegel" id="jp15_megan_cover">
+          <div class="carousel-caption carousel-caption-left">
+            <h3>Megan Spiegel</h3>
+          </div>
+        </div>
+        <div class="item">
+          <img src="http://lifepointe.starvertellc.netdna-cdn.com/wp-content/uploads/jp15_blank_cover.jpg" alt="Whitney Paxton" id="jp15_whitney_cover">
+          <div class="carousel-caption carousel-caption-right">
+            <h3>Whitney Paxton</h3>
+          </div>
+        </div>
+        <div class="item">
+          <img src="http://lifepointe.starvertellc.netdna-cdn.com/wp-content/uploads/jp15_blank_cover.jpg" alt="Wiliawan Northrop" id="jp15_wiliawan_cover">
+          <div class="carousel-caption carousel-caption-right">
+            <h3>Wiliawan Northrop</h3>
+          </div>
+        </div>
+        <div class="item">
+          <img src="http://lifepointe.starvertellc.netdna-cdn.com/wp-content/uploads/jp15_josiah_cover.jpg" alt="Josiah Burke" id="jp15_josiah_cover">
+          <div class="carousel-caption carousel-caption-right">
+            <h3>Josiah Burke</h3>
+          </div>
+        </div>
+        <div class="item">
+          <img src="http://lifepointe.starvertellc.netdna-cdn.com/wp-content/uploads/jp15_matt_cover.jpg" alt="Matt Beall" id="jp15_matt_cover">
+          <div class="carousel-caption carousel-caption-left">
+            <h3>Matt Beall</h3>
+          </div>
+        </div>
+      </div>
+
+      <!-- Controls -->
+      <a class="left carousel-control" href="#carousel-jp15" role="button" data-slide="prev">
+        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="right carousel-control" href="#carousel-jp15" role="button" data-slide="next">
+        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
+    </div>
+
+    <div class="banner">
+      <p>LifePointe Church is sending a team to Tokyo, Japan<br>to serve alongside Janet Brown and the local Zao Church</p>
+      <a class="btn btn-link" href="#meet-the-team">Meet the Team</a> <a class="btn btn-link" href="#pledge-support">Pledge Support</a> <a class="btn btn-link" href="#follow">Follow Us</a>
+    </div>
+
+    <div class="container">
+      <div class="row">
+        <h2 class="col-xs-12" id="meet-the-team">Meet the Team</h2>
+      </div>
+      <div class="row">
+        <div class="jp-profile col-xs-12 col-md-6">
+          <div class="row">
+            <h3 class="col-xs-12">Matt Beall</h3>
+            <div class="jp-profile-left col-xs-4">
+              <img class="jp-profile-img" src="http://lifepointe.starvertellc.netdna-cdn.com/wp-content/uploads/jp15_matt_profile.jpg" alt="Matt Beall">
+              <a class="btn btn-blue btn-block" href="#pledge-support">Pledge support</a>
+            </div>
+            <div class="jp-profile-content col-xs-8">
+              <p>Matt is a recent graduate from CSU, and the Founder and President of Star Verte LLC, a graphic design and website development firm. He loves others through service, and sees this trip as an opportunity for spiritual growth and extending God's love to Japan.</p>
+              <p><strong>Prayer requests</strong>
+                <ul>
+                  <li>Self-discipline and spiritual growth</li>
+                  <li>All barriers to going on the trip be removed</li>
+                  <li>No problems arise with Star Verte LLC before or during the trip</li>
+                </ul>
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="jp-profile col-xs-12 col-md-6">
+          <div class="row">
+            <h3 class="col-xs-12">Josiah Burke</h3>
+            <div class="jp-profile-left col-xs-4">
+              <img class="jp-profile-img" src="http://lifepointe.starvertellc.netdna-cdn.com/wp-content/uploads/jp15_josiah_profile.jpg" alt="Josiah Burke">
+              <a class="btn btn-blue btn-block" href="#pledge-support">Pledge support</a>
+            </div>
+            <div class="jp-profile-content col-xs-8">
+              <p>Josiah is a 20 year old college student studying Criminal Justice. He is going to Japan, following Jesus' commission to make disciples in all nations. Josiah hopes to gain a new perspective on humanity in general and discover if God is calling him to lifelong missions.</p>
+              <p><strong>Prayer requests</strong>
+                <ul>
+                  <li>Raise necessary funds</li>
+                  <li>Finish spring semester well, allowing more time to prepare for trip</li>
+                  <li>Be a good steward for Christ</li>
+                </ul>
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="jp-profile col-xs-12 col-md-6">
+          <div class="row">
+            <h3 class="col-xs-12">Wiliawan Northrop</h3>
+            <div class="jp-profile-left col-xs-4">
+              <img class="jp-profile-img" src="http://lifepointe.starvertellc.netdna-cdn.com/wp-content/uploads/jp15_blank_profile.jpg" alt="Wiliawan Northrop">
+              <a class="btn btn-blue btn-block" href="#pledge-support">Pledge support</a>
+            </div>
+            <div class="jp-profile-content col-xs-8">
+              <p></p>
+              <p><strong>Prayer requests</strong>
+                <ul>
+                  <li></li>
+                </ul>
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="jp-profile col-xs-12 col-md-6">
+          <div class="row">
+            <h3 class="col-xs-12">Whitney Paxton</h3>
+            <div class="jp-profile-left col-xs-4">
+              <img class="jp-profile-img" src="http://lifepointe.starvertellc.netdna-cdn.com/wp-content/uploads/jp15_blank_profile.jpg" alt="Whitney Paxton">
+              <a class="btn btn-blue btn-block" href="#pledge-support">Pledge support</a>
+            </div>
+            <div class="jp-profile-content col-xs-8">
+              <p>Whitney is majoring in Humanities with an emphasis in Asian Studies at the University of Colorado in Boulder. The Japanese have one of the most beautiful countries and cultures. She wants to spread God's love, and let them know the hope and joy that comes from Jesus.</p>
+              <p><strong>Prayer requests</strong>
+                <ul>
+                  <li>Be joyful and open to new experiences</li>
+                  <li>Be brave and given the words from the Holy Spirit to share her faith</li>
+                  <li>The Japanese will see God through her and the team</li>
+                </ul>
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="jp-profile col-xs-12 col-md-6">
+          <div class="row">
+            <h3 class="col-xs-12">Megan Spiegel</h3>
+            <div class="jp-profile-left col-xs-4">
+              <img class="jp-profile-img" src="http://lifepointe.starvertellc.netdna-cdn.com/wp-content/uploads/jp15_megan_profile.jpg" alt="Megan Spiegel">
+              <a class="btn btn-blue btn-block" href="#pledge-support">Pledge support</a>
+            </div>
+            <div class="jp-profile-content col-xs-8">
+              <p>Megan married her husband, Josh, in 2007 and they have a daughter AdiLynn, who is a precious part of their family. They enjoy camping, snowboarding, hunting, fishing, playing games and photography. Megan loves and is certain God will change her life, and strengthen her relationship with Him through this trip.</p>
+              <p><strong>Prayer requests</strong>
+                <ul>
+                  <li>Josh and AdiLynn</li>
+                  <li>Humility, energy, and safety</li>
+                  <li>God's presence and guidance</li>
+                  <li>Inspiration from the Holy Spirit to explain the gospel clearly</li>
+                  <li>Opportunities for the gospel to be heard</li>
+                </ul>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <hr>
+
+      <div class="row">
+        <h2 class="col-xs-12" id="pledge-support">Pledge Support</h2>
+      </div>
+
+      <div class="progress" id="jp-progress">
+        <div class="progress-bar progress-bar-danger" role="progressbar" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100" style="width: 10%;">
+          10% funds raised
+        </div>
+      </div>
+      <div class="row">
+        <small class="col-xs-12 text-right">Last updated: 4/26/2015</small>
+      </div>
+      <div class="row">
+        <p class="col-xs-12">Coming soon.</p>
+      </div>
+
+      <hr>
+
+      <div class="row">
+        <h2 class="col-xs-12" id="follow">Follow Us</h2>
+        <p class="col-xs-12">Coming soon.</p>
+      </div>
+    </div>
+
+  </div><!-- #primary -->
+
+<?php flint_get_widgets('footer'); ?>
+<?php get_footer(); ?>
