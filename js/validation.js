7a0efb5158d2e8e3a1ce831ea81d6b3c6e36f548
Matt Beall <mbeall@starverte.com>
2015-05-04 14:38:56 -0600

    Update validation.js for Japan page

--- js/validation.js
+++ js/validation.js
@@ -1,13 +1,10 @@
-function steel_validate() {
+function steel_validate($extend) {
   'use strict';
   var $success = true;
   var $alert = '<div class="alert alert-danger"><ul>';
 
-  function steel_validate_field($required) {
-    if (undefined === $required) {
-      $required = jQuery(this).data('required');
-    }
-
+  function steel_validate_field() {
+    var $required = jQuery(this).data('required');
     var $target = jQuery(this).data('target');
     var $data = jQuery($target).val();
     var $title = $target.substr(1);
@@ -34,13 +31,45 @@ function steel_validate() {
         else {
           jQuery($target).val($amount);
         }
-
       }
     }
   }
 
   jQuery('*').removeClass('has-error');
   jQuery('.form-validate').each(steel_validate_field);
+
+  /** Specific to Trinity - Japan template
+   *
+   * @todo Remove and add ability to customize
+   */
+  console.log('Financial Support is ' + jQuery('#financial_support').prop('checked'));
+  if (true === jQuery('#financial_support').prop('checked')) {
+    var $required = true;
+    var $target = jQuery('#amt_dec');
+    var $data = jQuery($target).val();
+    var $title = 'Gift Amount';
+
+    if (!$data) {
+      if (true === $required) {
+        $success = false;
+        jQuery('#amt_dec_group').addClass('has-error');
+        $alert += '<li>' + $title + ' is required.</li>';
+      }
+    }
+    else {
+      if ('currency' === jQuery('#amt_dec_group').data('type')) {
+        var $amount = Number($data).toFixed(2);
+        if (true === $required && 0 >= $amount) {
+          $success = false;
+          jQuery('#amt_dec_group').addClass('has-error');
+          $alert += '<li>' + $title + ' must be greater than $0.00.</li>';
+        }
+        else {
+          jQuery($target).val($amount);
+        }
+      }
+    }
+  }
 
   if (false === $success) {
     $alert += '</ul></div>';
