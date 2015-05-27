73f7dae3d9a9caa215d40bb5c7b77380578fe18c
Matt Beall <mbeall@starverte.com>
2015-05-26 20:13:06 -0600

    Banner-style overlays for front page 'next steps'

--- js/front-page.js
+++ js/front-page.js
@@ -7,6 +7,29 @@ jQuery( document ).ready(function() {
   jQuery('#church_profile_container').css('margin-top', overlay_margin_top);
   jQuery('#church_profile_clearfix').css('margin-top', clearfix_margin_top);
 });
+jQuery('#serve_img').load(function() {
+  jQuery( '.ns-caption' ).each(function() {
+    img = jQuery(this).data('img');
+    caption = jQuery(this).data('caption');
+
+    img_height = jQuery(img).height();
+    caption_height = jQuery(caption).height();
+    caption_width = jQuery(caption).width();
+
+    margin_top = (Math.round(0.5 * img_height) + Math.round(0.5 * caption_height)) * -1;
+    if (jQuery(window).width() < 768) {
+      margin_bottom = Math.round(0.5 * img_height);
+    }
+    else {
+      margin_bottom = 0;
+    }
+
+    jQuery(this).css('margin-top', margin_top);
+    jQuery(this).css('margin-bottom', margin_bottom);
+    jQuery(this).css('width', caption_width + caption_height);
+    jQuery('#next-steps .col-sm-4').css('min-height', img_height);
+  });
+});
 jQuery( window ).resize(function() {
   window_width = jQuery(window).width();
   overlay_height = jQuery('#church_profile_overlay').height();
@@ -15,4 +38,26 @@ jQuery( window ).resize(function() {
 
   jQuery('#church_profile_container').css('margin-top', overlay_margin_top);
   jQuery('#church_profile_clearfix').css('margin-top', clearfix_margin_top);
+
+  jQuery( '.ns-caption' ).each(function() {
+    img = jQuery(this).data('img');
+    caption = jQuery(this).data('caption');
+
+    img_height = jQuery(img).height();
+    caption_height = jQuery(caption).height();
+    caption_width = jQuery(caption).width();
+
+    margin_top = (Math.round(0.5 * img_height) + Math.round(0.5 * caption_height)) * -1;
+    if (jQuery(window).width() < 768) {
+      margin_bottom = Math.round(0.5 * img_height);
+    }
+    else {
+      margin_bottom = 0;
+    }
+
+    jQuery(this).css('margin-top', margin_top);
+    jQuery(this).css('margin-bottom', margin_bottom);
+    jQuery(this).css('width', caption_width + caption_height);
+    jQuery('#next-steps .col-sm-4').css('min-height', img_height);
+  });
 });
